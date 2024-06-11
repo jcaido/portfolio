8a89a77fc92dd6339f03ba212c0a78aa4c5746b9
Cristobal Rosa <crisrogar@gmail.com>
2024-06-11 13:11:33 +0200

    create project-modal

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,14 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../_models/Project';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ProjectModalComponent } from '../project-modal/project-modal.component';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
   imports: [],
   templateUrl: './project-card.component.html',
-  styleUrl: './project-card.component.css'
+  styleUrl: './project-card.component.css',
+  providers: [BsModalService]
 })
 export class ProjectCardComponent {
   @Input() project = {} as Project;
+  bsModalRef?: BsModalRef;
+
+  constructor(private modalService: BsModalService) {
+
+  }
+
+  OpenProjectModal() {
+    this.bsModalRef = this.modalService.show(ProjectModalComponent);
+  }
 
 }
---
+++ src/app/project-modal/project-modal.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-project-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-modal.component.html',
+  styleUrl: './project-modal.component.css'
+})
+export class ProjectModalComponent {
+
+  constructor(public bsModalRef: BsModalRef) {
+
+  }
+
+}
