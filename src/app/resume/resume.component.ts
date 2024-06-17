0308d9b1dd0bfbc1265ab970859dc7354bffc84e
Cristobal Rosa <crisrogar@gmail.com>
2024-06-17 18:34:33 +0200

    other parts of resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,14 +1,20 @@
 import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
+
+  isWorkExperienceOpen: boolean = false;
+  isEducationOpen: boolean = false;
+  isCertificationsOpen: boolean = false;
+  isSkillsOpen: boolean = false;
 
   constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle('Cristóbal Rosa - resume')
