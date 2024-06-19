f34b548e129474251cb9b0ec5f7044cee96f7be3
Cristobal Rosa <crisrogar@gmail.com>
2024-06-19 19:38:38 +0200

    update portfolio page

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,15 +1,14 @@
 export class Tag {
-  static readonly ANGULAR = new Tag('Angular', 'red');
-  static readonly TYPESCRIPT = new Tag('TypeScript', 'darkred');
-  static readonly PYTHON = new Tag('Python', 'pink');
-  static readonly CSHARP = new Tag('C#', 'green');
-  static readonly JAVA = new Tag('Java', 'orange');
-  static readonly NODEJS = new Tag('Node.js', 'brown');
-  static readonly ASPNET = new Tag('ASP.NET', 'purple');
-  static readonly JAVASCRIPT = new Tag('JavaScript', 'orange');
-  static readonly REACT = new Tag('React', 'blue');
+  static readonly ANGULAR = new Tag('Angular', '#C50836', 'assets/angular.svg');
+  static readonly TYPESCRIPT = new Tag('TypeScript', '#3178C6', 'assets/typescript.svg');
+  static readonly JAVA_SPRING_BOOT = new Tag('Java Spring Boot', '#69B729', 'assets/spring.svg');
+  static readonly JAVASCRIPT = new Tag('JavaScript', '#F7DF1E', 'assets/javascript.svg');
+  static readonly REACT = new Tag('React', '#66DBFB', 'assets/react.svg');
+  static readonly POSTGRESQL = new Tag('PostgreSql', '#396C94', 'assets/postgreSql.svg');
+  static readonly DOCKER = new Tag('Docker', '#0895E7', 'assets/docker1.svg');
 
-  private constructor(private readonly key: string, public readonly color: string) {
+
+  private constructor(private readonly key: string, public readonly color: string, public readonly icon: string) {
 
   }
 
